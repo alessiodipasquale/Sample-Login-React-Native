f2b14c18b1882f3c2879f586a207cb95047d6190
alessiodipasquale <alessiodipasquale2000@gmail.com>
2019-08-02 14:58:50 +0200

    Added working login with Node server

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,8 +1,62 @@
 import React, { Component } from 'react';
 
 import {Text, View, TextInput, StyleSheet, TouchableOpacity, StatusBar} from 'react-native';
+import {AsyncStorage} from 'react-native';
+import {ToastAndroid} from 'react-native';
+
+
 
 export default class LoginForm extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            email : '',
+            password : '',
+        }
+    }
+
+    _storeData = async (token) => {
+        try {
+          await AsyncStorage.setItem('token', token);
+          ToastAndroid.show('Login riuscito !', ToastAndroid.SHORT);
+
+        } catch (error) {
+            ToastAndroid.show('Errore nel salvataggio !', ToastAndroid.SHORT);
+
+        }
+      };
+
+    _onPressButton = () => {
+        let data = {
+            method: 'POST',
+            body: JSON.stringify({
+              email: this.state.email,
+              password: this.state.password
+            }),
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+          }
+          try {
+          return fetch('http://192.168.1.112:3000/login', data)
+                .then(response => {
+                    if (response.status == '200') {
+                        response.json()
+                        .then(json => this._storeData(json.token))
+                    } else {
+                        ToastAndroid.show('Credenziali errate !', ToastAndroid.SHORT);                    }
+                }) // promise
+                  
+          } catch (error) {
+            ToastAndroid.show('Errore nella richiesta !', ToastAndroid.SHORT);
+
+          }
+           
+    }
+
+    
+
   render() {
     return (
       <View style={styles.container}>
@@ -13,13 +67,15 @@ export default class LoginForm extends Component {
            keyboardType="email-address"
            autoCapitalize="none"
            autoCorrect={false}
+           onChangeText={(email) => this.setState({email: email})}
            />
           <TextInput style={styles.input}
            placeholder="Password"
            placeholderTextColor="#000"
            secureTextEntry
+           onChangeText={(password) => this.setState({password: password})}
            />
-           <TouchableOpacity style={styles.buttonContainer}>
+           <TouchableOpacity onPress={this._onPressButton} style={styles.buttonContainer}>
                <Text style={styles.buttonText}>
                 Accedi
                </Text>
@@ -28,6 +84,8 @@ export default class LoginForm extends Component {
     );
   }
 }
+
+
 
 const styles = StyleSheet.create({
     container : {
